7194c32ac3d534cdf286df5ea6b8f8f197ba570c
msekulovic <milorad.sekulovic@gmail.com>
2017-01-04 12:54:05 +0100

    First Commit

---
+++ app/src/main/java/me/infiniteimmagionation/fuellog/DateViewActivity.java
@@ -0,0 +1,13 @@
+package me.infiniteimmagionation.fuellog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DateViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_view);
+    }
+}
---
+++ app/src/main/java/me/infiniteimmagionation/fuellog/LastNActivity.java
@@ -0,0 +1,13 @@
+package me.infiniteimmagionation.fuellog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LastNActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last_n);
+    }
+}
