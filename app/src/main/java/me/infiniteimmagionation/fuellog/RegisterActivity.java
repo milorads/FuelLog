d1d7249e644f96ee20cd83c030e0730c3ff37290
msekulovic <milorad.sekulovic@gmail.com>
2017-01-04 17:16:09 +0100

    Changed the register look

--- app/src/main/java/me/infiniteimmagionation/fuellog/RegisterActivity.java
+++ app/src/main/java/me/infiniteimmagionation/fuellog/RegisterActivity.java
@@ -1,13 +1,25 @@
 package me.infiniteimmagionation.fuellog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
+        Button saveDugme = (Button)findViewById(R.id.saveButton);
+        saveDugme.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.saveButton){
+
+        }
     }
 }
